d5a37ed9d8f995babe7ba947a0c120abb48c226a
shimingzhi0017 <shimingzhi0017@gmail.com>
2026-09-15 00:10:51 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -26,12 +26,12 @@ for group, url in sources.items():
             if not line:
                 continue
 
-            # 去掉 # 后面的备注
             item = line.split("#")[0].split(",")[0].strip()
 
             count += 1
 
-            yaml_content += f"""
+            yaml_content += (
+f"""
   - name: {group}{count}
     type: vless
     server: {item}
@@ -48,6 +48,7 @@ for group, url in sources.items():
       headers:
         Host: shimingzhi0017.xyz
 """
+            )
 
             if count >= 5:
                 break
